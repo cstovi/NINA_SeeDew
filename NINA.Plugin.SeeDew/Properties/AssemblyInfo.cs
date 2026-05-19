a7c60bdf24a4b2f7fb64f3dc4ef4e021c4108c4d
Carl Stovell <carlstovell@gmail.com>
2026-05-19 18:40:18 +0100

    Add Ko-fi support link to plugin page description and bump version to 1.5.0

--- NINA.Plugin.SeeDew/Properties/AssemblyInfo.cs
+++ NINA.Plugin.SeeDew/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("SeeDew")]
 [assembly: AssemblyCopyright("Copyright © 2026 Carl Stovell")]
 [assembly: Guid("1B307E89-2157-4DFB-BFCA-A3ED3A58E272")]
-[assembly: AssemblyVersion("1.4.1.0")]
-[assembly: AssemblyFileVersion("1.4.1.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
 
 [assembly: AssemblyMetadata("License", "MPL-2.0")]
 [assembly: AssemblyMetadata("LicenseURL", "https://www.mozilla.org/MPL/2.0/")]
